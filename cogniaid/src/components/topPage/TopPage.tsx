411406a99993c58d948cf55d75861e73c50ab1ae
Kaikei-e <e_kaikei@protonmail.com>
2023-03-05 03:05:12 +0900

    Update: styling logo

--- cogniaid/src/components/topPage/TopPage.tsx
+++ cogniaid/src/components/topPage/TopPage.tsx
@@ -3,7 +3,7 @@ import styles from '../../styles/topPage/TopPage.module.css';
 const TopPage = () => {
   return (
     <div class={styles.bgPreset}>
-      <h1>TopPage</h1>
+      <h1 class={styles.logo}>Weathering: Flow, Adapt, Overcome.</h1>
     </div>
   );
 };
