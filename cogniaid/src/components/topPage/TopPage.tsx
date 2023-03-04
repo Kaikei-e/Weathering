2ce3ae94088250ca0bdb6f6f002bffeda5339304
Kaikei-e <e_kaikei@protonmail.com>
2023-03-05 02:52:35 +0900

    Update: styled landing page

---
+++ cogniaid/src/components/topPage/TopPage.tsx
@@ -0,0 +1,12 @@
+import styles from '../../styles/topPage/TopPage.module.css';
+
+const TopPage = () => {
+  return (
+    <div class={styles.bgPreset}>
+      <h1>TopPage</h1>
+    </div>
+  );
+};
+
+
+export default TopPage;
