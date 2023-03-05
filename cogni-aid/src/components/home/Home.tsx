c428f012932ba2de1752a575145dd7d3cad93056
Kaikei-e <e_kaikei@protonmail.com>
2023-03-05 22:56:59 +0900

    Update: Add link to the cogniAid

--- cogni-aid/src/components/home/Home.tsx
+++ cogni-aid/src/components/home/Home.tsx
@@ -1,9 +1,16 @@
+import { A } from '@solidjs/router';
 import styles from './Home.module.css';
 
 const HomeIsland = () => {
   return (
-    <div>
+    <div class={styles.layout}>
       <h1 class={styles.title}>Home</h1>
+      <p class={styles.description}> Welcome to Weathering !</p>
+      <div class={styles.islandLayout}>
+        <A href='/home/cogni-aid' class={styles.cogniAid}>
+          Cogni Aid: A cognitive tuning for mood intensity.
+        </A>
+      </div>
     </div>
   );
 };
--- cogni-aid/src/routes/home/index.tsx
+++ cogni-aid/src/routes/home/index.tsx
@@ -3,7 +3,7 @@ import styles from "./index.module.css"
 
 const Home = () => {
   return (
-    <div class={styles.bgStyles}>
+    <div class={styles.bgStyle}>
       <HomeIsland />
     </div>
   );
