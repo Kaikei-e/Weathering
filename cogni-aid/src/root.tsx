558acd601269284cf88ca2d045f36b0882d41757
Kaikei-e <e_kaikei@protonmail.com>
2023-03-05 20:45:55 +0900

    Update: making Home page

--- cogni-aid/src/root.tsx
+++ cogni-aid/src/root.tsx
@@ -22,7 +22,7 @@ export default function Root() {
           <Meta name="viewport" content="width=device-width, initial-scale=1" />
           <Link rel="preconnect" href="https://fonts.googleapis.com" />
           <Link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin=""/>
-          <Link href="https://fonts.googleapis.com/css2?family=Philosopher&display=swap" rel="stylesheet" />
+        <Link href="https://fonts.googleapis.com/css2?family=Cormorant&display=swap" rel="stylesheet" />
         </Head>
         <Body>
           <Suspense>
