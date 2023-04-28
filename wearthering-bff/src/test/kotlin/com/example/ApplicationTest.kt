2579658d8be9c08925d058f1433456b6343a2553
Kaikei-e <e_kaikei@protonmail.com>
2023-04-29 00:52:17 +0900

    weathering-bff: init bff repo

---
+++ wearthering-bff/src/test/kotlin/com/example/ApplicationTest.kt
@@ -0,0 +1,26 @@
+package com.example
+
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.server.testing.*
+import kotlin.test.*
+import io.ktor.http.*
+import com.example.plugins.*
+
+class ApplicationTest {
+    @Test
+    fun testRoot() = testApplication {
+        application {
+            configureRouting()
+        }
+        client.get("/").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+
+        client.get("/api/v1/forecast").apply {
+            assertEquals(HttpStatusCode.OK, status)
+            assertEquals("Hello World!", bodyAsText())
+        }
+    }
+}
