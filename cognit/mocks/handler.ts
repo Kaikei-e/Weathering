e777f238eebabf6142e725c1f56b8a74a40ae86c
Kaikei-e <e_kaikei@protonmail.com>
2023-04-29 01:52:45 +0900

    weathering-bff: add system pings

--- cognit/mocks/handler.ts
+++ cognit/mocks/handler.ts
@@ -2,6 +2,7 @@ import { rest } from "msw";
 
 export const handlers = [
   rest.get("/api/system/ping", (req, res, ctx) => {
-    return res(ctx.status(200));
+    
+    return res(ctx.status(200), ctx.body(JSON.stringify({ message: "pong" })));
   }),
 ];
--- cognit/src/pages/api/system/ping.ts
+++ cognit/src/pages/api/system/ping.ts
@@ -1,7 +1,18 @@
+import axios from "axios";
 import { NextApiRequest, NextApiResponse } from "next";
 
 const checkHealth = async (req: NextApiRequest, res: NextApiResponse) => {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/system/ping`);
-  const data = await response.json();
-  res.status(200).json(data);
-}
+  // const response = await fetch(
+  //   `${process.env.NEXT_PUBLIC_API_URL}/api/system/ping`
+  // );
+  // const data = await response.json();
+  // res.status(200).json(data);
+  
+  const response = await axios.get(
+    `${process.env.NEXT_PUBLIC_API_URL}/api/system/ping`
+  );
+  res.status(200).json(response.data);
+  
+};
+
+export default checkHealth;
