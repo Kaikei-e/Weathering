daa6dd2ce85aa34ca06ec3016c50e8c0140d94d4
Kaikei-e <e_kaikei@protonmail.com>
2023-04-29 13:28:12 +0900

    cognit: fmt all

--- cognit/mocks/handler.ts
+++ cognit/mocks/handler.ts
@@ -2,7 +2,6 @@ import { rest } from "msw";
 
 export const handlers = [
   rest.get("/api/system/ping", (req, res, ctx) => {
-    
     return res(ctx.status(200), ctx.body(JSON.stringify({ message: "pong" })));
   }),
 ];
--- cognit/src/pages/api/system/ping.ts
+++ cognit/src/pages/api/system/ping.ts
@@ -7,12 +7,11 @@ const checkHealth = async (req: NextApiRequest, res: NextApiResponse) => {
   // );
   // const data = await response.json();
   // res.status(200).json(data);
-  
+
   const response = await axios.get(
     `${process.env.NEXT_PUBLIC_API_URL}/api/system/ping`
   );
   res.status(200).json(response.data);
-  
 };
 
 export default checkHealth;
