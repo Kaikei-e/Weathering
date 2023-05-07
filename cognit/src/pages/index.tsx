eb2ea7eadcbb921fa1588bc4c45f28159b988544
Kaikei-e <e_kaikei@protonmail.com>
2023-05-07 16:51:54 +0900

    cognit: styling the statement

--- cognit/src/pages/index.tsx
+++ cognit/src/pages/index.tsx
@@ -7,28 +7,43 @@ const Home = () => {
       <h1
         css={css`
           position: absolute;
-          top: 80%;
-          left: 20%;
+          top: 75%;
+          left: 30%;
           transform: translate(-50%, -50%);
           color: white;
           font-size: 32px;
           text-align: center;
           text-shadow: 2px 2px 2px black;
-          font-family: "Roboto", sans-serif;
-          font-weight: 300;
+          font-weight: 400;
           letter-spacing: 0.1rem;
+          margin-bottom: 2%;
         `}
       >
         Weathering
       </h1>
-      <p>
-        Weathering is an application that helps you flow with, adapt to, and
-        overcome the waves of your emotions.
-      </p>
-      <p>
-        It assists you in regaining control and a sense of mastery over your
-        feelings.
-      </p>
+      <div css={
+        css`
+        width: 40%;
+          position: absolute;
+          top: 85%;
+          left: 30%;
+          transform: translate(-50%, -50%);
+          color: white;
+          font-size: 20px;
+          text-align: center;
+          text-shadow: 2px 2px 2px black;
+          font-weight: 300;
+        `
+      }>
+        <p>
+          Weathering is an application that helps you flow with, adapt to, and
+          overcome the waves of your emotions.
+        </p>
+        <p>
+          It assists you in regaining control and a sense of mastery over your
+          feelings.
+        </p>
+      </div>
     </div>
   );
 };
