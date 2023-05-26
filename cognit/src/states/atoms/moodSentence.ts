c54296b2422babd8929cea6631b03e2a66d8bdb3
Kaikei-e <e_kaikei@protonmail.com>
2023-05-27 00:05:58 +0900

    Cognit: learning Jotai. Stacked.

--- cognit/src/states/atoms/moodSentence.ts
+++ cognit/src/states/atoms/moodSentence.ts
@@ -1,11 +1,14 @@
-import {Getter, Setter, WritableAtom} from "jotai";
+import {atom, Getter, Setter, WritableAtom} from "jotai";
 
-export const adultSentenceAtom: WritableAtom<string, string[], string[]> = atom([""], [""]);
+
+export const sentenceAtom = atom([""])
+export const adultSentenceAtom = atom(
+    (get) => get(sentenceAtom),
+    (get, set, newSentence) => {
+        set(sentenceAtom, newSentence)
+    }
+)
 export const childSentenceAtom: WritableAtom<string, string[], string[]> = atom([""]);
 export const parentSentenceAtom : WritableAtom<string, string[], string[]> = atom([""]);
 export const moodSentenceAtom = atom([adultSentenceAtom, childSentenceAtom, parentSentenceAtom]);
 
-function atom<Value, Update>(
-    read: (get: Getter) => Value | Promise<Value>,
-    write: (get: Getter, set: Setter, update: Update) => void | Promise<void>
-): WritableAtom<Value,  Update>
